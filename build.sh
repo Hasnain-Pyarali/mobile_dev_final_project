d33823d58be69118a52839b02ac78f19c74d9f96
ange <ange@yw5n.com>
2025-05-27 04:02:58 +0000

    feat: build apk + github actions

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+IMG=git.gmoker.com/icing/flutter:main
+
+docker volume create --ignore flutter_cache
+set -x
+#docker run --entrypoint bash --rm -v "flutter_cache:/root/" -v "/dev/bus/usb/:/dev/bus/usb/" -v "$PWD:/app/" "$IMG" -c 'sleep 1 && flutter --no-version-check run -v'
+docker run --rm \
+    -v "flutter_cache:/root/" \
+    -v "$PWD:/app/" \
+    "$IMG" \
+    build apk -v --release
